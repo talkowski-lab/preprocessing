9e5ea18e79f6ab5811fba28b1d9c68ab239383a3
Eren Shin <eshin0525@gmail.com>
2025-09-03 19:03:32 -0400

    remove parallel="separate_header" and merging shards (not compressed)

--- scripts/hail_relatedness_check_v0.1.py
+++ scripts/hail_relatedness_check_v0.1.py
@@ -230,7 +230,8 @@ downsampled_unrelated = unrelated_in_ped.filter(unrelated_in_ped.relationship=='
 
 rel_total = related_in_ped.union(only_related).union(downsampled_unrelated)
 
-rel_total.export(f"{cohort_prefix}_kinship.tsv.gz", parallel='separate_header')
+# Export as file chunks
+rel_total.export(f"{cohort_prefix}_kinship.tsv.gz")
 
 # rel_df = rel_total.to_pandas()
 # rel_df.to_csv(f"{cohort_prefix}_kinship.tsv.gz", sep='\t', index=False)
